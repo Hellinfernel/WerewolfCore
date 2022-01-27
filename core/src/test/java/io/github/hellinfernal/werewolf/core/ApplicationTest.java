534314b20ee09ef65f94c29b749c38da34771a18
Aleksandar Damjanovic <codejanovic@gmail.com>
2022-01-27 17:22:46 +0100

    initial project setup

---
+++ core/src/test/java/io/github/hellinfernal/werewolf/core/ApplicationTest.java
@@ -0,0 +1,13 @@
+package io.github.hellinfernal.werewolf.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ApplicationTest {
+
+    @Test
+    void name() {
+        assertThat(true).isTrue();
+    }
+}
