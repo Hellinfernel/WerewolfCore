4cb8b075cc74e7b3f34d9b0352335216c00d0a3c
Aleksandar Damjanovic <codejanovic@gmail.com>
2022-02-25 17:01:58 +0100

    added discord exmaple & bot project

---
+++ discord-bot-examples/src/main/java/io/github/hellinfernal/werewolf/discord/examples/Main.java
@@ -0,0 +1,9 @@
+package io.github.hellinfernal.werewolf.discord.examples;
+
+public class Main {
+    // Wie und wann kommunizieren wir mit dem User?
+    // -> DM ? Channel ? Vote ? Reactions?
+    public static void main(String[] args) {
+
+    }
+}
---
+++ discord-bot/src/main/java/io/github/hellinfernal/werewolf/discord/bot/Bot.java
@@ -0,0 +1,7 @@
+package io.github.hellinfernal.werewolf.discord.bot;
+
+public class Bot {
+    public static void main(String[] args) {
+
+    }
+}
